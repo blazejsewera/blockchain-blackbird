6cf60d3e5f4cc053dfa2e2f9afc595edf7682566
Blazej Sewera <code@sewera.dev>
2022-01-04 20:56:12 +0100

    feat: extend papi interface

--- src/bb/common/net/papi.py
+++ src/bb/common/net/papi.py
@@ -1,8 +1,14 @@
 from Pyro5 import api
 from Pyro5.client import Proxy, _RemoteMethod, _StreamResultIterator
 from Pyro5.protocol import ReceivingMessage
+from Pyro5.server import Daemon as PDaemon
+from Pyro5.server import expose
 
 ProxyProperty = ReceivingMessage | _StreamResultIterator | _RemoteMethod | None
+
+
+def locate_ns() -> Proxy:
+    return api.locate_ns()
 
 
 def invoke(method: ProxyProperty, *args, **kwargs):
@@ -13,7 +19,7 @@ def invoke(method: ProxyProperty, *args, **kwargs):
 
 
 def get_all_uris_as_dict(prefix: str) -> dict[str, str]:
-    ns: Proxy = api.locate_ns()
+    ns = locate_ns()
     return invoke(ns.list, prefix)
 
 
@@ -24,3 +30,35 @@ def get_all_uris(prefix: str) -> list[str]:
 
 def proxy_of(uri: str) -> Proxy:
     return Proxy(uri)
+
+
+class Daemon(PDaemon):
+    ns = locate_ns()
+
+    registered_names: list[str] = []
+
+    def register(self, obj_or_class: object, ns_name: str):
+        """Register an object or class in daemon and nameserver."""
+        uri = super().register(obj_or_class)
+        invoke(self.ns.register, ns_name, uri)
+        self.registered_names.append(ns_name)
+
+    def shutdown(self):
+        for rname in self.registered_names:
+            invoke(self.ns.remove, rname)
+            print(f"> removed {rname} from ns")
+        super().shutdown()
+
+    def start(self):
+        print(f"> starting daemon")
+        try:
+            super().requestLoop()
+        except Exception:
+            pass
+        finally:
+            print("\n> shutting down...")
+            self.shutdown()
+            print("> done")
+
+
+expose  # re-export
