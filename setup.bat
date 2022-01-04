27328ea1eb4dd86b3d16f557b6323a660a67c8cb
Mateusz Winnicki <01143867@pw.edu.pl>
2022-01-04 19:05:30 +0100

    Replace python3 with python

--- setup.bat
+++ setup.bat
@@ -2,7 +2,7 @@
 
 echo "Python version should be 3.10 or higher"
 mkdir -p ./venv
-python3 -m venv ./venv
+py -m venv ./venv
 
 call .\venv\bin\activate.bat
-pip3 install -r ./requirements.txt
+& C:/Users/48795/AppData/Local/Programs/Python/python10/python.exe -m pip install -r .\requirements.txt
