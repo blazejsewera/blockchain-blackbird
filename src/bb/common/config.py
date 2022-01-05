a34fefbe5d9fdc6ccc83b10e6a0a9caa8c8ecc7f
Blazej Sewera <blazejok1@wp.pl>
2022-01-05 21:07:52 +0100

    refactor: parametrize hashing algorithm, rename methods

--- src/bb/common/config.py
+++ src/bb/common/config.py
@@ -1,1 +1,5 @@
+from cryptography.hazmat.primitives.hashes import SHA256
+
 DEFAULT_ENCODING = "utf-8"
+DEFAULT_HASH = SHA256
+DEFAULT_RSA_PUBLIC_EXPONENT = 65537
--- src/bb/common/sec/asymmetric.py
+++ src/bb/common/sec/asymmetric.py
@@ -6,32 +6,38 @@ from cryptography.hazmat.primitives.serialization import (
     load_pem_public_key,
 )
 
+from bb.common.config import DEFAULT_HASH, DEFAULT_RSA_PUBLIC_EXPONENT
+
 from .encode import base64_str_to_bytes, bytes_to_base64_str, str_to_bytes
-from .hash import SHA256
 
 
 def __pss_padding():
-    return padding.PSS(mgf=padding.MGF1(SHA256()), salt_length=padding.PSS.MAX_LENGTH)
+    return padding.PSS(
+        mgf=padding.MGF1(DEFAULT_HASH()), salt_length=padding.PSS.MAX_LENGTH
+    )
 
 
 def generate_private_key() -> rsa.RSAPrivateKey:
-    DEFAULT_RSA_PUBLIC_EXPONENT = 65537
     return rsa.generate_private_key(DEFAULT_RSA_PUBLIC_EXPONENT, 1024)
 
 
-def sign(message: str, private_key: rsa.RSAPrivateKey) -> str:
+def sign_rsa_base64(message: str, private_key: rsa.RSAPrivateKey) -> str:
     message_bytes = str_to_bytes(message)
-    signed_bytes = private_key.sign(message_bytes, __pss_padding(), SHA256())
+    signed_bytes = private_key.sign(message_bytes, __pss_padding(), DEFAULT_HASH())
 
     return bytes_to_base64_str(signed_bytes)
 
 
-def verify(message: str, signature_base64: str, public_key: rsa.RSAPublicKey) -> bool:
+def verify_rsa(
+    message: str, signature_base64: str, public_key: rsa.RSAPublicKey
+) -> bool:
     verified = True
     signature_bytes = base64_str_to_bytes(signature_base64)
     message_bytes = str_to_bytes(message)
     try:
-        public_key.verify(signature_bytes, message_bytes, __pss_padding(), SHA256())
+        public_key.verify(
+            signature_bytes, message_bytes, __pss_padding(), DEFAULT_HASH()
+        )
     except InvalidSignature:
         verified = False
 
