e90b0a0a6264aad7249f83a4963d8e0d75c4400c
Blazej Sewera <blazejok1@wp.pl>
2022-01-05 21:42:50 +0100

    refactor: remove unnecessary pk generation methods from client

--- src/bb/client/client.py
+++ src/bb/client/client.py
@@ -5,14 +5,7 @@ from bb.common.net.papi import get_all_uris, invoke, proxy_of
 from bb.node.names import NODE_ENDPOINT
 
 
-def generate_pk():
-    DEFAULT_RSA_PUBLIC_EXPONENT = 65537
-    pk = rsa.generate_private_key(DEFAULT_RSA_PUBLIC_EXPONENT, 1024)
-    print(pk.public_key().public_bytes(Encoding.OpenSSH, PublicFormat.OpenSSH))
-
-
 def start():
-    generate_pk()
     print("\nRemote object:")
     node1_uri = get_all_uris(NODE_ENDPOINT)[0]
     node = proxy_of(node1_uri)
