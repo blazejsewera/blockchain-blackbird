a3c7f35b14661f4ec50e61fb66ade7373b95ab74
Mateusz Winnicki <01143867@pw.edu.pl>
2022-01-16 00:41:27 +0100

    feat: add proof_found machanism

--- src/bb/node/node.py
+++ src/bb/node/node.py
@@ -10,7 +10,7 @@ def start():
     daemon = Daemon()
     network = Network()
     endpoint = Endpoint(network)
-    node = Node()
+    node = Node(network)
 
     endpoint_name = f"{NODE_ENDPOINT}.{generate_guid()}"
     daemon.register(endpoint, endpoint_name)
